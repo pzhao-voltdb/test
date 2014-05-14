bf6db111d4945315d1d962912fdc58146a5bb2ec
pzhao-voltdb <pzhao@voltdb.com>
2014-05-14 11:44:56 -0400

    added yo.java

---
+++ yo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class yo
+{
+	public static void main( String[] args )
+	{
+		System.out.println("yo");
+//		System.out.print("ENTER YOUR PIN: ");
+//		System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+	}
+}
